925a1cc9d9772655757fca1e3c5dc56672455e17
raj <rkraj39888@gmail.com>
2022-09-10 20:36:23 -0700

    guess number game

--- guess_your_right_number.py.py
+++ guess_your_right_number.py.py
@@ -1,28 +1,26 @@
-import random
-randomnumber = random.randint(1,10  )
-userguess =  None
+import random 
+randomnumber =  random.randint(1, 50)
+userguess = None
 guess = 0
-while(userguess!=randomnumber):
-    userguess = int(input('enter the number:- '))
+
+while(userguess != randomnumber):
+    userguess = int(input('enter your guess number:- '))
+
     guess +=1
-    if(userguess == randomnumber):
-        print("thats great")
-    else:
-        if (userguess > randomnumber):
-            print("write lower")
-        else:
-            print("write upper")
-       
-
-print(f"you guess the number in {guess} th time")
-
-print(f'the number is {randomnumber}')
 
     
+    if (userguess == randomnumber):
+        print("you got your number")
 
+    else:
+        if(userguess > randomnumber):
+            print("give lower number as input")
+        else:
+            print("print higher number")
 
+with open('hiscore.txt', 'r') as f:
+    hiscore = int(f.read())
+with open('hiscore.txt', 'w') as f:
+    f.write(str(guess))
 
-
-
-
-
+print(f'your total gueses is {guess}')
