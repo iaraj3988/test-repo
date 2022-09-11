fb00ff4e2a17100435fd73e9188a8d7f156c7234
raj <rkraj39888@gmail.com>
2022-09-11 04:56:37 -0700

    guess number

--- practise.py
+++ practise.py
@@ -2,24 +2,22 @@ import random
 randomnumber =  random.randint(1, 100)
 userguess = None
 guess = 0
+try:
+    while(userguess!=randomnumber):
+        userguess = int(input("enter your number:- "))
+        guess +=1
 
-while(userguess!=randomnumber):
-    userguess = int(input("enter your number:- "))
-    guess +=1       
-    
-    if userguess == randomnumber:
-        print("this is your guess number")
-    else:
-        if(userguess>randomnumber):
-            print("please lower number")
+        if userguess == randomnumber:
+            print("this is your guess number")
         else:
-            print("please upper number ")
-
-
-print(f"your total guesses is {guess}")   
-
-
-print(f'the number is {randomnumber}')
+            if(userguess>randomnumber):
+                print("please lower number")
+            else:
+                print("please upper number ")
+except Exception as e:
+    print('exceptional  error please give input in number')
+print(f'your guesess is {guess}')
+print("thank you for playing!")
 
 with open("score.txt","r") as f:
     score = int(f.read())
@@ -27,3 +25,16 @@ if guess<score:
     print("you broke the score")
     with open("score.txt","w") as f:
         f.write(str(guess))
+
+
+
+      
+        
+
+    
+
+
+ 
+
+
+
